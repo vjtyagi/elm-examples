1b79f683069b7f678a13de50d08def9d2dd7ca64
vjunloc <vijaytyagi.vt@gmail.com>
2015-10-03 20:20:49 +0530

    created the counter example

--- Hello.elm
+++ Hello.elm
@@ -8,32 +8,84 @@ Learn about the Elm's core libraries:
 
 -}
 
-import Graphics.Element exposing (..)
+import Graphics.Element exposing (flow, down, Element, show)
+import String exposing (length)
+import List exposing (sum, map, length)
+
+fortyTwo : Int
+fortyTwo =
+  42
+
+myNames : List String
+myNames =
+  ["Alice", "Bob", "Chuck"]
+
+book : {title : String, author : String, pages : Int}
+book = { title = "Damian", author = "Hesse", pages = 123}
+
+
+averageNameLength : List String -> Int
+averageNameLength names =
+  List.sum ( List.map String.length names ) // List.length names
+
+isLong : {record | pages : Int } -> Bool
+
+isLong book =
+  book.pages > 400
+
+type Visibility = All | Active | Completed
+
+toString : Visibility -> String
+
+toString visibility =
+  case visibility of
+    All ->
+      "All"
+    Active ->
+      "Active"
+    Completed ->
+      "Completed"
+
+
+type User = Anonymous | LoggedIn String
+userPhoto : User -> String
+userPhoto user =
+  case user of
+    Anonymous ->
+      "anon.jpg"
+    LoggedIn name ->
+      "users/" ++ name ++ "/photo.png"
+
+activeUsers : List User
+activeUsers = 
+    [
+       Anonymous
+      ,LoggedIn "Tom"
+      ,LoggedIn "Steve"
+      ,Anonymous
+    ]
+
+type Widget =
+    ScatterPlot (List (Int Int))
+  | LogData (List String)
+  | TimePlot (List (Time, Int))
+
+
 
 
 main : Element
 main =
     flow down
       [ helloWorld
-      , welcomeGraphics
+      , demo
       ]
 
 
 helloWorld : Element
 helloWorld =
-    show "Hello, World!"
+    show "Hello, World!" 
 
+demo : Element
+demo = 
+  show (map userPhoto activeUsers)
 
-welcomeGraphics : Element
-welcomeGraphics =
-    let dimensions = 90
-        imgSize = 30
-        elmLogo =
-          image imgSize imgSize "http://elm-lang.org/logo.png"
-
-        elmsPerSide = dimensions // imgSize
-
-        row =
-          flow right (List.repeat elmsPerSide elmLogo)
-    in
-        flow down (List.repeat elmsPerSide row)
