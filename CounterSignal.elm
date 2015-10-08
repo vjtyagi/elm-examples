267d26860d7006280d709f75fa7dc7a41b1b707a
vjunloc <vijaytyagi.vt@gmail.com>
2015-10-08 22:53:54 +0530

    completed the implementation

--- CounterSignal.elm
+++ CounterSignal.elm
@@ -1,5 +1,5 @@
 import Counter exposing (update, view)
-
+import Debug
 main =
     start { model = 0, view = view, update = update }
 
@@ -8,3 +8,22 @@ type alias config model action =
     , view : Address action -> model -> Html
     , update: action -> model -> model
     }
+
+start : Config model action -> Signal Html
+start config =
+    let
+        actions =
+            Signal.mailbox Nothing
+        address =
+            Signal.forwardTo actions.address Just
+
+        update action model =
+            case action of
+                Just action ->
+                    config.update action model
+                Nothing ->
+                    Debug.crash "This shouldn't happen"
+        model = Signal.foldp update config.model actions.signal
+
+    in 
+        Signal.map (config.view address) model
